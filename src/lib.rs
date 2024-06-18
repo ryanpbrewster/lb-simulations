f125f4661dc3dcfcc2c64d953cfe1cdb0a36376d
Ryan P. Brewster <RyanPBrewster@gmail.com>
2024-06-18 16:03:55 -0400

    reframe as unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,102 +1,8 @@
+#![allow(dead_code)]
+
 use std::collections::BTreeMap;
 
-use clap::Parser;
 use rand::{rngs::SmallRng, Rng, SeedableRng};
-
-/*
-Sample output from
-$ cargo run --release -- --iterations=1000000
-
-[a] 0.99992
-[b] 1.00138
-[b] 0.99883
-[b] 1.00172
-[b] 0.99911
-[b] 0.99896
-[c] 1.00092
-[c] 0.99619
-[c] 1.00335
-[c] 1.00154
-[c] 0.99987
-[c] 1.00022
-[c] 0.99613
-[c] 1.00119
-[c] 1.00066
-% in-zone = 0.7333283333333334
-*/
-
-fn main() {
-    let args = Args::parse();
-
-    let backends: Vec<Backend> = {
-        let mut acc = Vec::new();
-        for _ in 0..1 {
-            acc.push(Backend {
-                id: acc.len() as u32,
-                zone: 'a',
-                capacity: 1.0,
-            });
-        }
-        for _ in 0..5 {
-            acc.push(Backend {
-                id: acc.len() as u32,
-                zone: 'b',
-                capacity: 1.0,
-            });
-        }
-        for _ in 0..9 {
-            acc.push(Backend {
-                id: acc.len() as u32,
-                zone: 'c',
-                capacity: 1.0,
-            });
-        }
-        acc
-    };
-    let mut clients: Vec<Client> = {
-        let mut acc = Vec::new();
-        acc.push(Client::new('a', backends.clone()));
-        acc.push(Client::new('b', backends.clone()));
-        acc.push(Client::new('c', backends.clone()));
-        // If there were a Zone D without any backends, clients in zones A..C won't even
-        // know it exists. That screws up their calculations and the overall
-        // distribution is skewed slightly. Uncomment this to see the skewed output.
-        // acc.push(Client::new('d', backends.clone()));
-        acc
-    };
-
-    let mut tally = vec![0; backends.len()];
-    let mut in_zone = 0;
-    let mut total = 0;
-    for client in &mut clients {
-        for _ in 0..args.iterations {
-            let b = client.sample() as usize;
-            tally[b] += 1;
-            if backends[b].zone == client.zone {
-                in_zone += 1;
-            }
-            total += 1;
-        }
-    }
-
-    for (backend, count) in backends.iter().zip(tally) {
-        println!(
-            "[{zone}] {frac:.05}",
-            zone = backend.zone,
-            frac = count as f64 / (total / backends.len()) as f64
-        );
-    }
-    println!(
-        "% in-zone = {fraction}",
-        fraction = in_zone as f64 / total as f64
-    );
-}
-
-#[derive(Parser)]
-struct Args {
-    #[arg(long, default_value_t = 1_000)]
-    iterations: u64,
-}
 
 #[derive(Clone)]
 struct Client {
@@ -164,10 +70,13 @@ impl Client {
             prng: SmallRng::seed_from_u64(42),
         }
     }
-    fn sample(&mut self) -> u32 {
+    fn sample(&mut self, p: fn(&Backend) -> bool) -> u32 {
         let mut cur = 0;
         let mut total_weight = 0.0;
         for b in &self.backends {
+            if !p(b) {
+                continue;
+            }
             let Some(&lambda) = self.zonal_multiplier.get(&b.zone) else {
                 continue;
             };
@@ -186,4 +95,95 @@ struct Backend {
     id: u32,
     zone: char,
     capacity: f64,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn naive() {
+        /*
+        Sample output from
+        $ cargo run --release -- --iterations=1000000
+
+        [a] 0.99992
+        [b] 1.00138
+        [b] 0.99883
+        [b] 1.00172
+        [b] 0.99911
+        [b] 0.99896
+        [c] 1.00092
+        [c] 0.99619
+        [c] 1.00335
+        [c] 1.00154
+        [c] 0.99987
+        [c] 1.00022
+        [c] 0.99613
+        [c] 1.00119
+        [c] 1.00066
+        % in-zone = 0.7333283333333334
+        */
+
+        let iterations = 100_000;
+        let backends: Vec<Backend> = {
+            let mut acc = Vec::new();
+            for _ in 0..1 {
+                acc.push(Backend {
+                    id: acc.len() as u32,
+                    zone: 'a',
+                    capacity: 1.0,
+                });
+            }
+            for _ in 0..5 {
+                acc.push(Backend {
+                    id: acc.len() as u32,
+                    zone: 'b',
+                    capacity: 1.0,
+                });
+            }
+            for _ in 0..9 {
+                acc.push(Backend {
+                    id: acc.len() as u32,
+                    zone: 'c',
+                    capacity: 1.0,
+                });
+            }
+            acc
+        };
+        let mut clients: Vec<Client> = {
+            let mut acc = Vec::new();
+            acc.push(Client::new('a', backends.clone()));
+            acc.push(Client::new('b', backends.clone()));
+            acc.push(Client::new('c', backends.clone()));
+            // If there were a Zone D without any backends, clients in zones A..C won't even
+            // know it exists. That screws up their calculations and the overall
+            // distribution is skewed slightly. Uncomment this to see the skewed output.
+            // acc.push(Client::new('d', backends.clone()));
+            acc
+        };
+
+        let mut tally = vec![0; backends.len()];
+        let mut in_zone = 0;
+        let mut total = 0;
+        for client in &mut clients {
+            for _ in 0..iterations {
+                let b = client.sample(|_| true) as usize;
+                tally[b] += 1;
+                if backends[b].zone == client.zone {
+                    in_zone += 1;
+                }
+                total += 1;
+            }
+        }
+
+        let avg = total as f64 / backends.len() as f64;
+        let min_load = tally.iter().min().copied().unwrap() as f64 / avg;
+        let max_load = tally.iter().max().copied().unwrap() as f64 / avg;
+
+        assert!(0.95 <= min_load, "min load = {min_load}");
+        assert!(max_load <= 1.05, "max load = {max_load}");
+
+        let in_zone_frac = in_zone as f64 / total as f64;
+        assert!(in_zone_frac > 0.70, "in_zone = {in_zone_frac}");
+    }
 }
